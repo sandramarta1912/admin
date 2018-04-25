1f0b78d7bda41f9223fdbaca442bdcbbf0c56c58
sandramarta1912 <sandramarta1912@gmail.com>
2018-04-25 15:11:27 +0300

    Add steps in jenkinsfile

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o admin .
 
-MYSQL_HOST=localhost MYSQL_PORT=3306 MYSQL_ROOT=root MYSQL_ROOT_PASSWORD=cms MYSQL_DATABASE=adserver ./admin
+MYSQL_HOST=localhost MYSQL_PORT=3306 MYSQL_ROOT=root MYSQL_ROOT_PASSWORD=cms MYSQL_DATABASE=adserver
